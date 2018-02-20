bb3fc0ea5ac56b579373ed16c012b65d14f9beab
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2018-02-20 00:35:21 -0300

    20/02/2018

--- projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -61,7 +61,7 @@ public class ProdutoDao {
 		query.where((Predicate[])predicates.toArray(new Predicate[0]));
 		
 		TypedQuery<Produto> typedQuery = em.createQuery(query);
-		
+		typedQuery.setHint("org.hibernate.cacheable", "true");
 		return typedQuery.getResultList();
 	}
 
--- projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
@@ -16,9 +16,12 @@ import javax.persistence.Version;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
+@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Produto {
 
 	@Id
@@ -45,6 +48,7 @@ public class Produto {
 	private Loja loja;
 	
 	@ManyToMany
+	@Cache(usage=CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	
